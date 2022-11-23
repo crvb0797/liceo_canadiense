1c7052cbb11dc396cc11cf4cdb54fe7ab61ff2e9
Carlos Villatoro <crvb0797@gmail.com>
2022-11-23 13:21:35 -0600

    Se repara el carusel de marcas

---
+++ assets/slider-brand.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    arrows: true,
+    dots: false,
+    pauseOnHover: false,
+    prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
+    nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 2
+      }
+    }]
+    });
+  });
